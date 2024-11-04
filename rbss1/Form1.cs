5815a80e18cb609ece11260f1164965b30dd052a
feuerdoge <deniboz11@gmail.com>
2024-11-04 14:41:52 +0100

    Felder Hinzugefügt
    Einfacher Algorithmus zur Felderstellung

--- rbss1/Form1.cs
+++ rbss1/Form1.cs
@@ -15,6 +15,36 @@ namespace rbss1
         public Form1()
         {
             InitializeComponent();
+            Feldgenerierung();
+        }
+        public void Feldgenerierung() 
+        {
+            int x = 0;
+            int y = 0;
+
+            for(int i = 0; i < 10; i++) 
+            {
+                Feld feld = new Feld();
+                feld.textur.Size = new Size(50, 50);
+                feld.textur.Location = new Point(x, y);
+                feld.textur.Image = Properties.Resources.squar2;
+                this.Controls.Add(feld.textur);
+                
+
+                for (int j = 0; j < 10; j++)
+                {
+                    Feld feldy = new Feld();
+                    feldy.textur.Size = new Size(50, 50);
+                    feldy.textur.Location = new Point(x, y);
+                    feldy.textur.Image = Properties.Resources.squar2;
+                    this.Controls.Add(feldy.textur);
+                    y += 50;
+                }
+                y = 0;
+                x += 50;
+
+            }
+            label1.Text = $"{this.Width}";
         }
     }
 }
