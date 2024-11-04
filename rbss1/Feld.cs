22c6c214a6154e6f88647ccafdc40c2b0ac1342d
feuerdoge <deniboz11@gmail.com>
2024-11-04 13:40:58 +0100

    Feldklasse erstellt
    Titel

---
+++ rbss1/Feld.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rbss1
+{
+    internal class Feld
+    {
+        public string besitzer;
+
+        public Feld(string besitzer)
+        {
+            this.besitzer = besitzer;
+        }
+
+    }
+}
