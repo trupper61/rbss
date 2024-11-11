5aa2b7e6d3b357f833e7bc4c244c373e22e7c1df
trupper61 <rupert.hoffmann.06@gmail.com>
2024-11-11 13:23:46 +0100

    Added Stadt WIP
    Stadt with no teritory
    WIP

---
+++ rbss1/Stadt.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rbss1
+{
+    public class Stadt
+    {
+        public List<Feld> stadtFlaeche { get; set; }
+        public Feld[,] worldMap { get; set; }
+        public PictureBox textur { get; private set; }
+
+        public Stadt(Feld startFeld, Feld[,] worldMap)
+        {
+            stadtFlaeche = new List<Feld>();
+            stadtFlaeche.Add(startFeld);
+            this.worldMap = worldMap;
+            textur = new PictureBox
+            {
+                Size = new Size(40, 40),
+                BackColor = Color.Green
+            };
+            InitializeStadt();
+        }
+        public void InitializeStadt()
+        {
+            Feld startFeld = stadtFlaeche[0];
+            int x = (startFeld.textur.Height - textur.Height) / 2;
+            int y = (startFeld.textur.Width - textur.Width) / 2;
+            textur.Location = new Point(startFeld.textur.Location.X + x, startFeld.textur.Location.Y + y);
+            textur.Image = Properties.Resources.rathaus;
+            textur.BringToFront();
+        }
+    }
+}
