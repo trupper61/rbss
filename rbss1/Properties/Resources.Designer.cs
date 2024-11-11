7bbc609ff67d45567d4583603e428592fbbcc245
feuerdoge <deniboz11@gmail.com>
2024-11-11 10:54:13 +0100

    Rathaus

--- rbss1/Properties/Resources.Designer.cs
+++ rbss1/Properties/Resources.Designer.cs
@@ -93,6 +93,16 @@ namespace rbss1.Properties {
         /// <summary>
         ///   Sucht eine lokalisierte Ressource vom Typ System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap rathaus {
+            get {
+                object obj = ResourceManager.GetObject("rathaus", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Sucht eine lokalisierte Ressource vom Typ System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap squar2 {
             get {
                 object obj = ResourceManager.GetObject("squar2", resourceCulture);
